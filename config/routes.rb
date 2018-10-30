0865a3cb7a9c6569ed86efd52642aab1f365cc97
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-30 16:57:54 -0700

    Config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'recipes/root'
+
+  root 'recipes#root'
+
   get 'recipes/index'
   get 'recipes/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
