b2463dff26ac69f95b18b300149838ea0dd03a80
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-30 15:34:09 -0700

    Created draft of routes/empty file for recipe and slack_api_wrapper

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,18 @@
+class RecipesController < ApplicationController
+  # What methods do we need?
+  # New for new search? - Make this root?
+  # Create to run the search/redirect to a list page?
+  # List for custom route to show list of recipes
+  # Show to show individual recipes
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'recipes#index'
+  get ':recipe/new', to: 'recipes#new', as: 'recipe_new' 
+  post ':recipe', to: 'recipes#create', as: 'recipe_create'
 end
