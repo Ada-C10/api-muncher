09ca9f822e49b8b9f514e7e14a24bb883f353d4e
jackie <jacquelyn.cheng@me.com>
2018-10-30 16:47:12 -0700

    Added form on homepage to take in search query for recipes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,15 @@
 class RecipesController < ApplicationController
   def index
-    @recipes = EdamamApiWrapper.list_recipes("cheese")
+    @recipes = EdamamApiWrapper.list_recipes(search_params)
+    @query = search_params
   end
 
   def show
   end
+
+  private
+
+    def search_params
+      return params.require(:query)
+    end
 end
