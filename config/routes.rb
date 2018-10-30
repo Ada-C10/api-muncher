5088decfcf3730a168a4847df7e9703f88c1e43a
Trang <trang.frego@gmail.com>
2018-10-30 15:51:10 -0700

    create Recipes controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'recipes/index', to: 'recipes#index', as: 'recipes'
+  get 'recipes/show', to: 'recipes#show', as: 'recipe'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
