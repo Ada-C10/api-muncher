0b2ff0d6860a3f02ba3dc8ca38fea8301ce015bd
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-31 10:29:50 -0700

    Add and configure VCR and Webmock

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
-ENV["RAILS_ENV"] = "test"
+ENV["RAILS_ENV"] = "test" # what's this?
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
 require "minitest/rails"
@@ -14,6 +14,21 @@ Minitest::Reporters.use!(
   Minitest.backtrace_filter
 )
 
+VCR.configure do |config|
+  config.cassette_library_dir = 'test/cassettes' # folder where casettes will be located
+  config.hook_into :webmock # tie into this other tool called webmock
+  config.default_cassette_options = {
+    :record => :new_episodes,    # record new data when we don't have it yet
+    :match_requests_on => [:method, :uri, :body] # The http method, URI and body of a request all need to match
+  }
+  # Don't leave our Edamam tokens lying around in a cassette file.
+  config.filter_sensitive_data("<EDAMAM_APP_ID>") do
+    ENV['EDAMAM_APP_ID']
+  end
+  config.filter_sensitive_data("<EDAMAM_APP_KEY>") do
+    ENV['EDAMAM_APP_KEY']
+  end
+end
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
