397385559473848d8e7c0d959511c51473fab377
BarbaraWidjono <barbarawidjono@gmail.com>
2018-10-31 15:08:49 -0700

    Class recipe created

---
+++ lib/recipe.rb
@@ -0,0 +1,13 @@
+class Recipe
+
+  def initialize(label, image, url, dietLabels, healthLabels, ingredientLines)
+    @label = label
+    @image = image
+    @url = url
+    @dietLabels = dietLabels
+    @healthLabels = healthLabels
+    @ingredientLines = ingredientLines
+
+  end
+
+end
