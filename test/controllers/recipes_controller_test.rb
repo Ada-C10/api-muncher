6fd384cdbfe8f588e4295aea4f90f286cf0258d5
Alice Hsiao <alice.hsiao87@gmail.com>
2018-10-31 15:43:51 -0700

    Completed Recipes Controller Testing

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,24 +1,45 @@
 require "test_helper"
 
 describe RecipesController do
-  it "should get index" do
-    get recipes_index_url
-    value(response).must_be :success?
+  it "can get the index page with a valid search term" do
+    VCR.use_cassette('list_of_recipes') do
+      search = "salmon"
+      get recipes_path, params: {search: search}
+
+      must_respond_with :success
+    end
   end
 
-  it "should get show" do
-    get recipes_show_url
-    value(response).must_be :success?
+  it "respond with an error message with an invalid search term" do
+    VCR.use_cassette('list_of_recipes') do
+      search = "faskjl134"
+      get recipes_path(search)
+
+      expect(flash[:status]).must_equal :warning
+      expect(flash[:message]).must_equal "No results for your search."
+      must_respond_with :redirect
+      must_redirect_to root_path
+    end
   end
 
-  it "should get new" do
-    get recipes_new_url
-    value(response).must_be :success?
+  it "can get the show page with a valid recipe id" do
+    VCR.use_cassette('single_recipe') do
+      recipe_id = "7bf4a371c6884d809682a72808da7dc2"
+      get recipe_path(recipe_id)
+
+      must_respond_with :success
+    end
   end
 
-  it "should get create" do
-    get recipes_create_url
-    value(response).must_be :success?
-  end
+  it "respond with an error message with an invalid recipe id" do
+    VCR.use_cassette('single_recipe') do
+      recipe_id = "7bf4a371c6884d809682a72808da7dc3"
+      get recipe_path(recipe_id)
 
+      expect(flash[:status]).must_equal :warning
+      expect(flash[:message]).must_equal "This recipe does not exist."
+      must_respond_with :redirect
+      must_redirect_to root_path
+    end
+  end
 end
