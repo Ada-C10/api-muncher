11a9f633695059445eafa1d5524b0147f877fc0e
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-31 14:52:40 -0700

    Rename constant

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -1,8 +1,7 @@
 require 'httparty'
 
 class EdamamApiWrapper
-
-  URL = "https://api.edamam.com/search"
+  BASE_URL = "https://api.edamam.com/search"
   APP_ID = ENV["APP_ID"]
   APP_KEY = ENV["APP_KEY"]
 end
