48301b808c8d010f72387806e57859f9d5590019
Madaleine Shields <maddiev.shields@gmail.com>
2018-10-31 14:58:45 -0700

    Begin defining self methods

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -4,4 +4,13 @@ class EdamamApiWrapper
   BASE_URL = "https://api.edamam.com/search"
   APP_ID = ENV["APP_ID"]
   APP_KEY = ENV["APP_KEY"]
+
+  def self.search(term)
+  end
+
+  def self.list_recipes
+  end
+
+  def self.show_recipe
+  end
 end
