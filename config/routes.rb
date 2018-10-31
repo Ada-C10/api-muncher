cd421d4e1645e5dc0bf353bd3979568e5e72ae2b
Kat Perreira <kat.perreira@gmail.com>
2018-10-30 17:05:36 -0700

    set up controleller and some lib files including the api_wrapper

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'search#index'
+
+  get ':hits/new', to: 'search#new', as: 'search_new'
+  post ':hits', to: 'search#create', as: 'search_create'
 end
