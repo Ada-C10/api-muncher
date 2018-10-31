c5929480e25b26af4662b07919f4dafc5ecdf39d
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-31 11:25:40 -0700

    Wrote controller tests for recipe_search search index show, failures

--- test/controllers/recipe_search_controller_test.rb
+++ test/controllers/recipe_search_controller_test.rb
@@ -2,18 +2,27 @@ require "test_helper"
 
 describe RecipeSearchController do
   it "should get search" do
-    get recipe_search_search_url
-    value(response).must_be :success?
+    get root_path
+    must_respond_with :success
   end
 
   it "should get index" do
-    get recipe_search_index_url
-    value(response).must_be :success?
+    VCR.use_cassette('list_recipes_file') do
+      get recipe_searches_path
+      must_respond_with :success
+    end
   end
 
-  it "should get show" do
-    get recipe_search_show_url
-    value(response).must_be :success?
+  it "should get show with valid ID" do
+    fragment = "recipe_3d81878cd040aa6a73a2c3f11293102a"
+    get recipe_path(fragment)
+    must_respond_with :success
+  end
+
+  it "should not get show with invalid ID" do
+    fragment = "invalid"
+    get recipe_path(fragment)
+    must_respond_with :not_found
   end
 
 end
