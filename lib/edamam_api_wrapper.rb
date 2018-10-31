d345d0a2939a6fb589302b4107d760a09868d1e2
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-10-30 17:46:03 -0700

    continued work on recipe creation method and index

---
+++ lib/edamam_api_wrapper.rb
@@ -0,0 +1,48 @@
+require 'httparty'
+
+class EdamamApiWrapper
+
+  BASE_URL = 'https://api.edamam.com/search'
+  TOKEN = ENV['EDAMAM_TOKEN']
+  ID = ENV['EDAMAM_ID']
+
+  def self.list_recipes(search_terms)
+    url = "#{BASE_URL}?q=#{search_terms}&app_id=#{ID}&app_key=#{TOKEN}"
+    data = HTTParty.get(url)
+    binding.pry
+    recipe_list = []
+    if data["hits"]
+      binding.pry
+      data["hits"].each do |recipe_data|
+        recipe_list << create_recipe(recipe_data["recipe"])
+      end
+      binding.pry
+    end
+    return recipe_list
+  end
+
+  def self.list_recipe(recipe_uri)
+    # Get rid of odd characters
+    encoded_uri = URI.encode(recipe_uri)
+    # Send the request
+    HTTParty.get(encoded_uri).parsed_response
+
+  end
+
+  private
+
+  def self.create_recipe(api_params)
+    return Recipe.new(
+      api_params["label"],
+      api_params["image"],
+      api_params["uri"],
+      api_params["url"],
+      api_params["ingredients"],
+      api_params["dietLabels"]
+    )
+
+  end
+
+  # http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_7bf4a371c6884d809682a72808da7dc2
+  # Swap out end with the number of the recipe somehow
+end
--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,18 +1,21 @@
 class Recipe
-  attr_reader :label, :image, :uri
+  attr_reader :name, :image, :uri, :url, :ingredients, :dietLabels
 
-  def initialize(name, image, uri, options = {})
-    
-    raise ArgumentError if name == nil || name.empty?
+  def initialize(label, image, uri, url, ingredients, dietLabels, options = {})
+
+    raise ArgumentError if label == nil || label.empty?
     raise ArgumentError if image == nil || image.empty?
     raise ArgumentError if uri == nil || uri.empty?
+    raise ArgumentError if url == nil || url.empty?
+    raise ArgumentError if ingredients == nil || ingredients.empty?
+    raise ArgumentError if dietLabels == nil || dietLabels.empty?
 
     @name = label
     @image = image
-    @uid = uid
-
-
-    # options here
+    @uri = uri
+    @url = url
+    @ingredients = ingredients
+    @dietLabels = dietLabels
 
   end
 
