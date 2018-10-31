60d4bee970492f9073abb346f83d8c310cee08ec
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-10-31 15:23:37 -0700

    Controller tests pass

--- app/controllers/recipe_search_controller.rb
+++ app/controllers/recipe_search_controller.rb
@@ -14,7 +14,7 @@ class RecipeSearchController < ApplicationController
     else
       flash[:status] = :failure
       flash[:message] = "It's not the most urgent thing in the world, but there was an issue with your request and we couldn't find that recipe."
-      render 'layouts/not_found'
+      render 'layouts/not_found', status: :not_found
     end
   end
 end
--- test/controllers/recipe_search_controller_test.rb
+++ test/controllers/recipe_search_controller_test.rb
@@ -13,20 +13,22 @@ describe RecipeSearchController do
     end
   end
 
-  # it "should get show with valid Edamam uri (ID)" do
-  #   fragment = "recipe_3d81878cd040aa6a73a2c3f11293102a"
-  #   VCR.use_cassette('get_recipe_file') do
-  #     get recipe_path(fragment)
-  #     must_respond_with :success
-        # expect(flash[:status]).must_equal :success
-  #   end
-  # end
+  it "should get show with valid Edamam uri (ID)" do
+    fragment = "recipe_3d81878cd040aa6a73a2c3f11293102a"
+    VCR.use_cassette('get_recipe_file') do
+      get recipe_path(fragment)
+      must_respond_with :success
+        expect(flash[:status]).must_equal :success
+    end
+  end
 
-  # it "should not get show with an invalid Edamam uri (ID)" do
-  #   fragment = "invalid"
-  #   get recipe_path(fragment)
-  #   must_respond_with :not_found
-  #   expect(flash[:status]).must_equal :failure
-  # end
+  it "should not get show with an invalid Edamam uri (ID)" do
+    fragment = "invalid"
+    VCR.use_cassette('get_recipe_file') do
+      get recipe_path(fragment)
+      must_respond_with :not_found
+      expect(flash[:status]).must_equal :failure
+    end
+  end
 
 end
