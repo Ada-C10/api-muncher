288a278c660d7c7edd4b1f6bc7889f1ac172db7a
Maryam Shitu <maryamshitu@gmail.com>
2018-10-31 13:19:49 -0700

    environment and app set up

---
+++ lib/edamam_api_wrapper.rb
@@ -0,0 +1,12 @@
+require 'httparty'
+
+class EdamamApiWrapper
+  BASE_URL = 'https://api.edamam.com/search'
+  TOKEN = ENV['EDAMAM_TOKEN']
+  APP_ID = ENV['EDAMAM_APP_ID']
+
+  def self.search_recipes
+
+  end
+
+end
