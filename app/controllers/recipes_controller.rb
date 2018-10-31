58022677d600c3a4b49c46a278e88e60a9a3df99
Daniela Di Iorio <danielad@gmail.com>
2018-10-31 14:25:35 -0700

    recipescontroller index and show methods

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,10 +4,16 @@ require 'will_paginate/array'
   def index
     recipes = EdamamApiWrapper.recipe_search(params[:keyword])
     @recipes = recipes.paginate(:page => params[:page], :per_page => 10)
+    if !recipes.any?
+      render :index, status: :bad_request
+    end
   end
 
   def show
     @recipe = EdamamApiWrapper.show_recipe(params[:id])
+    if @recipe.nil?
+      render :show, status: :bad_request
+    end
   end
 
 end
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,7 +1,45 @@
 require "test_helper"
 
 describe RecipesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+
+  describe 'root' do
+    it 'succeeds' do
+    end
+  end
+  describe 'index' do
+    it 'succeeds with valid search keyword' do
+      VCR.use_cassette('recipe_search') do
+        get recipes_path, params: {keyword: 'tofu'}
+
+        must_respond_with :success
+      end
+    end
+
+    it 'responds with bad_request with invalid search keyword' do
+      VCR.use_cassette('recipe_search') do
+        get recipes_path, params: {keyword: 'qmwmemrmtmmtmy'}
+
+        must_respond_with :bad_request
+      end
+    end
+
+  end
+
+  describe 'show' do
+    it 'succeeds with valid id' do
+      VCR.use_cassette('show_recipe') do
+        get recipe_path("5c16802dd815e76ce94487b567073877")
+
+        must_respond_with :success
+      end
+    end
+
+    it 'responds with bad_request with invalid id' do
+      VCR.use_cassette('show_recipe') do
+        get recipe_path("absolutegarbage")
+
+        must_respond_with :bad_request
+      end
+    end
+  end
 end
