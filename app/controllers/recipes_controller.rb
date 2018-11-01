35600799f064cccd7c14dc9eed90694ff45258fc
Jessie Zhang <zhang_jessie2017@outlook.com>
2018-11-01 07:20:26 -0700

    set up the index page for search a recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,15 @@
 class RecipesController < ApplicationController
+
+   def home
+   end
+
+   def show
+     # @receipt = Recipe.find_by()
+   end
+
+   def index
+     @recipes = ApiMuncherWrapper.list_recipes(params[:name])
+   end
+
+
 end
