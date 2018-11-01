8e7030f911594cfca387cacb78783fa3efe51592
jackie <jacquelyn.cheng@me.com>
2018-10-31 21:49:29 -0700

    completed api wrapper testing

--- test/lib/edamam_api_wrapper_test.rb
+++ test/lib/edamam_api_wrapper_test.rb
@@ -1,35 +1,33 @@
 require 'test_helper'
 
-describe SlackApiWrapper do
-  it 'can list channels' do
-    VCR.use_cassette('list_channels_file') do
-      channels = SlackApiWrapper.list_channels # this makes an API call
-      # and checks list_channels_file to see if has made the call before
-      # the first time, no, so it makes the call and saves it in the file
-      # the second time, it has seen it before so it won't make the request
-      # instead, it'll replay the saved data
+describe EdamamApiWrapper do
+  it 'can list recipes' do
+    VCR.use_cassette('list_recipes') do
+      recipes = EdamamApiWrapper.list_recipes("cheese")
 
-      expect(channels.length).must_be :>, 0
-      channels.each do |channel|
-        expect(channel).must_respond_to :name
-        expect(channel).must_respond_to :id
+      expect(recipes.length).must_be :>, 0
+
+      recipes.each do |recipe|
+        expect(recipe).must_respond_to :label
+        expect(recipe).must_respond_to :image
+        expect(recipe).must_respond_to :id
       end
     end
   end
 
-  it 'can send messages to valid channels' do
-    VCR.use_cassette('send_msg') do
-      worked = SlackApiWrapper.send_msg('test-api-channel', 'THIS IS JACKIE IN TESTING')
+  it 'can create a recipe object given a valid id' do
+    VCR.use_cassette('display_recipe') do
+      recipe = EdamamApiWrapper.display_recipe('1ba32b41f9f507c048b50b1704b13b8f')
 
-      expect(worked).must_equal true
+      expect(recipe).must_be_instance_of Recipe
     end
   end
 
-  it 'cannot send messages to invalid channels' do
-    VCR.use_cassette('send_msg') do
-      worked = SlackApiWrapper.send_msg('really-bogus-api-testing', 'THIS IS JACKIE IN TESTING')
+  it 'cannot create a recipe object given an invalid id' do
+    VCR.use_cassette('display_recipe') do
+      recipe = EdamamApiWrapper.display_recipe('1ba32b41f9f507c048b50b1704b13b8e')
 
-      expect(worked).must_equal false
+      expect(recipe).must_be_nil
     end
   end
 end
