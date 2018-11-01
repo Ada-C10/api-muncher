341df92f637a3eba913d2a982e4233143d67ebe9
Kat Perreira <kat.perreira@gmail.com>
2018-11-01 12:45:30 -0700

    fixed issues with Bootsnap, sorry about the bad branch name

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,17 @@
 class SearchController < ApplicationController
+
+  def index
+    @hits = params[:hits]
+  end
+
+  def new
+    @hits = params[:hit]
+  end
+
+  def create
+    EdamamApiWrapper.search_recipes(params[:q])
+
+    redirect_to root_path
+  end
+
 end
---
+++ lib/edamam_api_wrapper.rb
@@ -0,0 +1,25 @@
+require 'httparty'
+
+class EdamamApiWrapper
+  # BASE_URL = 'https://api.edamam.com/search'
+  # ID = ENV["APP_ID"]
+  # KEY = ENV["APP_KEY"]
+
+
+
+  # def self.search_recipes(q)
+  #   url = BASE_URL + ID + KEY + q
+  #   response = HTTParty.post(url,
+  #     body: {
+  #       "q" => q,
+  #
+  #     },
+  #     headers: {
+  #       'Content-Type' => 'application/x-www-form-urlencoded'
+  #     }
+  #
+  #   )
+  #   return response
+  # end
+
+end
