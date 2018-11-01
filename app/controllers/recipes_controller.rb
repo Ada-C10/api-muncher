54332e0f003530aeaecef67b268c902626f79c78
jackie <jacquelyn.cheng@me.com>
2018-10-31 21:53:23 -0700

    Added flash messages

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,17 +2,21 @@ require 'will_paginate/array'
 
 class RecipesController < ApplicationController
   def index
-    @recipes = EdamamApiWrapper.list_recipes(search_params).paginate(page: params[:page], per_page: 10)
-    @query = search_params
+    @query = params[:query]
+    @recipes = EdamamApiWrapper.list_recipes(@query).paginate(page: params[:page], per_page: 10)
+
+    if @recipes.empty?
+      flash.now[:message] = "Could not find any recipes for '#{@query}'. Here are some yummy ones instead!"
+      @query = "yummy"
+      @recipes = EdamamApiWrapper.list_recipes(@query).paginate(page: params[:page], per_page: 10)
+    end
   end
 
   def show
     @recipe = EdamamApiWrapper.display_recipe(params[:id])
-  end
 
-  private
-
-    def search_params
-      return params.require(:query)
+    if @recipe.nil?
+      flash.now[:message] = "Recipe not found. Try another search!"
     end
+  end
 end
