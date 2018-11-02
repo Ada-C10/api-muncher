3c8c57ebf2abe051a60410c288d043d785e0b022
Sigrid Benezra <sdbenezra@gmail.com>
2018-11-01 18:35:03 -0700

    Fix typo in recipes_controller. Complete test for index method of recipes_controller.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,13 @@
 class RecipesController < ApplicationController
+
   def index
     @food_search = params[:food_search]
     @recipes = RecipeApiWrapper.list_recipes(@food_search)
+    if @recipes == []
+      flash[:status] = :not_found
+          flash[:result_text] = "There were no results for your search term. Please try again"
+      redirect_to root_path, status: :bad_request
+    end
   end
 
   def show
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
+require 'pry'
 
 describe RecipesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index" do
+    before do
+      @valid_food_search = "chicken"
+      @invalid_food_search = 9999
+    end
+
+    it "will return success when supplied a valid search term" do
+      VCR.use_cassette("recipes_contr") do
+        get recipe_list_path, params: {food_search: @valid_food_search}
+        must_respond_with :success
+      end
+    end
+
+    it "will return an error message when supplied an invalid search term" do
+      VCR.use_cassette("invalid_recipes_contr") do
+        get recipe_list_path, params: {food_search: @invalid_food_search}
+        must_respond_with :bad_request
+      end
+    end
+
+    it "will return an error message when supplied with no search term" do
+      VCR.use_cassette("none_recipes_contr") do
+        get recipe_list_path, params: {food_search: nil}
+        must_respond_with :bad_request
+      end
+    end
+  end
 end
