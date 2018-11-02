0bfce9c1913a80730c64f146b6ad488054307a9a
Madaleine Shields <maddiev.shields@gmail.com>
2018-11-02 11:51:10 -0700

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   root 'recipes#root'
 
-  get 'recipes/index'
-  get 'recipes/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :recipes, only: [:index, :show]
+
 end
