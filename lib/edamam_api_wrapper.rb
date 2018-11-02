287eea112304c2a130c6438cac5897268879d5c2
Madaleine Shields <maddiev.shields@gmail.com>
2018-11-02 11:51:58 -0700

    def search and show methods

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -5,12 +5,43 @@ class EdamamApiWrapper
   APP_ID = ENV["APP_ID"]
   APP_KEY = ENV["APP_KEY"]
 
-  def self.search(term)
+
+  @recipe_list = []
+
+  def self.search(query)
+    if !@recipe_list.empty?
+      @recipe_list = []
+    end
+
+    encoded_uri =  URI.encode("#{BASE_URL}?q=#{query}&app_id=#{APP_ID}&app_key=#{APP_KEY}&from=0&to=30")
+    response = HTTParty.get(encoded_uri)
+
+    if response ["hits"]
+      response["hits"].each do |recipe|
+        @recipe_list << Recipe.new(recipe["recipe"])
+      end
+    end
+    return @recipe_list
   end
 
-  def self.list_recipes
-  end
+  def self.show_recipe(uri)
+    recipe_uri_list = []
 
-  def self.show_recipe
+    @recipe_list.each do |recipe|
+      recipe_uri_list << recipe.uri
+    end
+
+    if !recipe_uri_list.include?(uri)
+      encoded_uri = URI.encode("#{URL}?r=http://www.edamam.com/ontologies/edamam.owl#recipe_#{uri}&app_id=#{APP_ID}&app_key=#{APP_KEY}")
+
+      response = HTTParty.get(encoded_uri)
+      return Recipe.new(response[0])
+    end
+
+    @recipe_list.each do |recipe|
+      if recipe.uri == uri
+        return recipe
+      end
+    end
   end
 end
