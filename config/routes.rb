64efb2d0382f1dee61d55ce14279d67987956af4
Karis Kim <ouniiq@hotmail.com>
2018-11-01 22:07:27 -0700

    fixed routes issue and added form to new view

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   root 'recipes#new'
   get 'recipes/index', to: 'recipes#index', as: 'recipes'
-  get ':recipe/new', to: 'recipes#new', as: 'recipe_new'
+  get 'recipe/new', to: 'recipes#new', as: 'recipe_new'
   post ':recipe', to: 'recipes#create', as: 'recipe_create'
-  get ':recipe/show', to: 'recipes#show', as: 'recipe_create'
+  get ':recipe/show', to: 'recipes#show', as: 'recipe_show'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
