d0addf8e8dfe5669bcb91c0447dcb1d4162f9df1
Katie Jahanmir <katietj@gmail.com>
2018-11-02 11:16:42 -0700

    worked on views and added js to help edamam badge

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,13 @@
 class RecipesController < ApplicationController
   def index
+    @search = params[:label]
+    #@recipes = EdamamApiWrapper.list_recipes(params[:label]).paginate(:page => params[:page], :per_page => 10)
     @recipes = EdamamApiWrapper.list_recipes(params[:label])
   end
 
   def show
     @recipe = EdamamApiWrapper.single_recipe(params[:id])
-  
+
   end
 
   def search
@@ -13,8 +15,9 @@ class RecipesController < ApplicationController
 
 
   def create
-
   end
+
+
 
 
 end
