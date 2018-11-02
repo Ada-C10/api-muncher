35ec47e3002790d2b8baff1ad1b14a2085f3552c
BarbaraWidjono <barbarawidjono@gmail.com>
2018-11-02 11:51:04 -0700

    Change Recipes#find_recipes

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -14,6 +14,7 @@ class EdamamApiWrapper
     response = HTTParty.get(encoded_url)
 
     recipe_list = []
+    # if (response.status === 200)
     if response
       index = 0
       while index < response["hits"].length do
