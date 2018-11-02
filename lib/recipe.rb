b6eb4b8de5fe5d4f0518c0e82ae546ca3016ecf2
Semret Nicodimos <snicodimos@gmail.com>
2018-11-01 20:51:42 -0700

    appended the recipe.rb file of the initiliaze method

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -2,13 +2,12 @@ class Recipe
   attr_reader :label, :uri, :image, :url, :yield, :dietLabels, :healthLabels, :ingredientLines
 
   def initialize(recipe_data)
-    raise ArgumentError if recipe_data.nil?
+    raise ArgumentError if recipe_data.nil? || recipe_data[:label].nil? 
 
 
     @label = recipe_data[:label]
     @uri = recipe_data[:uri]
     @image = recipe_data[:image]
-
     @url = recipe_data[:url]
     @yield = recipe_data[:yield]
     @dietLabels = recipe_data[:dietLabels]
