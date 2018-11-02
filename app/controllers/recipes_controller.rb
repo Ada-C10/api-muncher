39404b68d51b57dc47c919321fda86c2e13ed53e
Kay <kayxn23@gmail.com>
2018-11-01 22:27:08 -0700

    recipes controller 4 tests created and passing

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,23 +3,15 @@ class RecipesController < ApplicationController
 
   def index
     search_term = params[:search_term] #the params come from the index form
-    if !search_term.empty?
+    if !search_term.nil?
       recipes_array = EdamamApiWrapper.list_recipes(search_term)
       if !recipes_array.empty?
-        # @recipes =  recipes_array.page(10)
-        # @recipes = Kaminari.paginate_array(recipes_array).page(1).per(10)
         @recipes = Kaminari.paginate_array(recipes_array).page(params[:page]).per(10)
       else
-        flash.now[:result_text] = "Recipes matching the search '#{search_term}' were not found, please try again!"
+        # redirect_back fallback_location: root_path, status: :bad_request
+        flash.now[:result_text] = "Recipes matching the search '  #{search_term} ' were not found, please try again!"
       end
-    else
-      flash.now[:result_text] = "Oops! a blank search. Please try again"
-      render :index
-
-      # redirect_back fallback_location: root_path, status: :bad_request
     end
-    # binding.pry
-    #need to add some conditions around this
   end
 
   def show
@@ -27,7 +19,7 @@ class RecipesController < ApplicationController
     @recipe = EdamamApiWrapper.find_recipe_by(params[:uri])
     if @recipe.nil?
       flash.now[:result_text] = "We're sorry this recipe's show page couldn't be found, please try another recipe!"
-      render :index
+      render :index, status: :bad_request
       # redirect_back fallback_location: root_path, status: :bad_request
     end
   end
--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -2,15 +2,48 @@ require "test_helper"
 
 describe RecipesController do
 
-  # it "can get the index path" do
-  #   VCR.use_cassette('list_channels_file') do
-  #
-  #     get root_path
-  #
-  #     must_respond_with :ok
-  #   end
-  # end
-  #
+  #should I test for home or root?
+
+  it "can get the root path" do
+    VCR.use_cassette('show_rootpage_file') do
+
+      get root_path
+
+      must_respond_with :ok
+    end
+  end
+
+  it "can get the index path " do
+    VCR.use_cassette('list_recipes_file') do
+
+      get search_path
+
+      must_respond_with :ok
+    end
+  end
+
+  it "can get the show path with valid uri" do
+    VCR.use_cassette('show_recipe_file') do
+
+      uri = "7543ecfa28b7506a97360748f017a83e"
+
+      get recipe_path(uri)
+
+      must_respond_with :ok
+    end
+  end
+
+  it "cannot get the show path with invalid uri" do
+    VCR.use_cassette('show_recipe_file') do
+
+      uri = "bogus7a83e"
+
+      get recipe_path(uri)
+
+      must_respond_with :bad_request
+    end
+  end
+
   # it "can get the new message form" do
   #   get chat_new_path("nodes-api-testing")
   #
