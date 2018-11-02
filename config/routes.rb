546f9d1342386dffd586e4332e99c2f5fa997f0b
Semret Nicodimos <snicodimos@gmail.com>
2018-11-01 20:50:51 -0700

    changes some routes to make app work properly.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'recipes#search', as: 'home'
+  root 'recipes#new'
 
-  resources :recipes
+  resources :recipes, only: [:show]
+
+  post 'recipes', to: 'recipes#index'
+
+  # get ':recipe/new', to: 'recipes#new', as: 'recipe_new'
+
+  # post ':recipes', to: 'recipes#create', as: 'recipe_create'
 
 end
