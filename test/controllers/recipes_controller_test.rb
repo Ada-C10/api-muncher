75656fd7eb26bfad95eaafe84d14bedf321629e3
Hannah <hannahvanseeters@gmail.com>
2018-11-01 19:06:10 -0700

    edited recipe controller test to account for invalid id

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -4,16 +4,24 @@ describe RecipesController do
   it "should get index" do
     VCR.use_cassette('list_recipes') do
       get recipes_path
-      # value(response).must_be :success?
+      
       must_respond_with :ok
     end
   end
 
-  it "should get show" do
+  it "should get show for valid id" do
     VCR.use_cassette('show_recipe') do
       get recipe_path("d87818beb641e9da9e122fb8cf69ab50")
 
       must_respond_with :ok
+    end
+  end
+
+  it "responds with not_found for invalid ID" do
+    VCR.use_cassette('show_recipe') do
+      get recipe_path(-1)
+
+      must_respond_with :not_found
     end
   end
 end
