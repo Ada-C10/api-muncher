30d1384409b547bf9ab809ce35a7c4880b2b5ae2
Xtina <2006peacegypsy@gmail.com>
2018-11-02 18:38:11 -0700

    added form-with helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
-require "minitest/rails"    
+require "minitest/rails"
 require "minitest/reporters"  # for Colorized output
 #  For colorful output!
 Minitest::Reporters.use!(
@@ -21,5 +21,9 @@ Minitest::Reporters.use!(
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  def form_with(options)
+  options[:local] = true
+  super options
+end
   # Add more helper methods to be used by all tests here...
 end
