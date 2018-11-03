5c23604e60081464eacd75e0936387b510a51af9
Valerie Gidzinski <valgidzi@gmail.com>
2018-11-03 15:38:26 -0700

    Add and run EdamamApiWrapper test, generate recipes cassette test file

--- test/lib/edamam_api_wrapper_test.rb
+++ test/lib/edamam_api_wrapper_test.rb
@@ -3,7 +3,13 @@ require 'test_helper'
 describe EdamamApiWrapper do
 
   describe 'search' do
-    
+    it 'can retrieve data' do
+      VCR.use_cassette("recipes") do
+        string = 'teriyaki'
+        response = EdamamApiWrapper.search(string)
+        response["count"].must_equal 3388
+      end
+    end
   end
 
 end
