a3bbd7abedb32c87e4f3894df30e11631ba006cd
BarbaraWidjono <barbarawidjono@gmail.com>
2018-11-02 21:55:07 -0700

    Paginating an array set up

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,21 +1,23 @@
 class RecipesController < ApplicationController
 
-  # Ruby gem: "willpaginate"
+
   def finder
     food_to_search = params[:food_type]
     redirect_to recipes_path(food_to_search)
   end
 
+  # Pagination on an array see: Gemfile (bottom), config/initializers/will_paginate_array_fix.rb, homepage/index.html.erb and index method
   def index
     @food_to_search = params[:food]
-    @matching_recipes = EdamamApiWrapper.find_recipes(@food_to_search)
+    recipe_list = EdamamApiWrapper.find_recipes(@food_to_search)
+    @matching_recipes = recipe_list.paginate(:page => params[:page], :per_page => 10)
 
     # Covering status codes 401, 404
-    if @matching_recipes == nil
+    if recipe_list == nil
       flash[:error] = "Ooops. Something happened. Please try again."
       redirect_back(fallback_location: root_path)
     # Covering status code 200 but no recipes are returned
-    elsif @matching_recipes.length == 0
+    elsif recipe_list.length == 0
       flash[:error] = "Ooops. Looks like there are no recipes for #{@food_to_search}. Please try again."
       redirect_back(fallback_location: root_path)
     # Covering status code 200 and recipes are found
