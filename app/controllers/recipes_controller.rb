5213c594d7833b9e1cfe371382885fe2e87ec27a
BarbaraWidjono <barbarawidjono@gmail.com>
2018-11-02 20:45:55 -0700

    Error messages when no search results found

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,23 +3,22 @@ class RecipesController < ApplicationController
   # Ruby gem: "willpaginate"
   def finder
     food_to_search = params[:food_type]
-    redirect_to recipes_path(food_to_search)
+    if food_to_search == ""
+      flash[:error] = "Ooops. Looks like you forgot to enter a food type. Please try again."
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_to recipes_path(food_to_search)
+    end
   end
 
   def index
     @food_to_search = params[:food]
     @matching_recipes = EdamamApiWrapper.find_recipes(@food_to_search)
 
-    # Covering status codes 401, 404
-    if @matching_recipes == nil
-      flash[:error] = "Ooops. Something happened. Please try again."
+    # Covering status codes 401, 404 and 200(when no recipes are found)
+    if @matching_recipes == nil || @matching_recipes.length == 0
+      flash[:error] = "Ooops. Looks like there are no recipes for '#{@food_to_search}'. Please try again."
       redirect_back(fallback_location: root_path)
-    # Covering status code 200 but no recipes are returned
-    elsif @matching_recipes.length == 0
-      flash[:error] = "Ooops. Looks like there are no recipes for #{@food_to_search}. Please try again."
-      redirect_back(fallback_location: root_path)
-    # Covering status code 200 and recipes are found
-    else
     end
   end
 
