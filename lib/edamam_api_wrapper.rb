239f548705edd74f4823da11b7fcbdbe791cdaf0
Valerie Gidzinski <valgidzi@gmail.com>
2018-11-04 10:47:00 -0800

    Add private .create_recipe method, use to refactor .search and .find_recipe methods

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -9,16 +9,43 @@ class EdamamApiWrapper
     url = BASE_URL + "q=#{string}&" + "app_id=#{ID}&" + "app_key=#{KEY}&" + "to=100"
 
     response = HTTParty.get(url)
-    return response["hits"]
+    recipe_list = []
+    if response["hits"]
+      response["hits"].each do |recipe_data|
+        recipe_list << create_recipe(recipe_data["recipe"])
+      end
+    end
+    return recipe_list
+
+    # return response["hits"]["recipe"]
   end
 
   def self.find_recipe(id)
     uri = "http://www.edamam.com/ontologies/edamam.owl#recipe_" + "#{id}"
     encoded_uri = URI.encode(uri)
     url = BASE_URL + "r=#{encoded_uri}&" + "app_id=#{ID}&" + "app_key=#{KEY}"
-    
+
     response = HTTParty.get(url)
-    return response
+    if response[0]
+      recipe = create_recipe(response[0])
+    end
+    return recipe
+    # return response[0]
+  end
+
+  private
+
+  def self.create_recipe(api_params)
+    return Recipe.new(
+      api_params["label"],
+      {
+        uri: api_params["uri"],
+        source: api_params["source"],
+        image: api_params["image"],
+        ingredients: api_params["ingredientLines"],
+        diet_labels: api_params["dietLabels"]
+      }
+    )
   end
 
 end
