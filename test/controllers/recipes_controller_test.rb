e779f2a5e984ea399c298836aca2285b40b97537
BarbaraWidjono <barbarawidjono@gmail.com>
2018-11-03 20:28:14 -0700

    Recipes#index test

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,7 +1,25 @@
 require "test_helper"
 
 describe RecipesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "finder" do
+    it 'informs user of mistake if an empty search string is submitted' do
+      food_type = {
+        food_type: ""
+      }
+
+      post find_path, params: food_type
+      expect(flash[:error]).must_equal "Ooops. Looks like you forgot to enter a food type. Please try again."
+    end
+
+    it 'redirects to Recipes#index if the user enters something in the search field' do
+      food_type = {
+        food_type: "apple"
+      }
+
+      post find_path, params: food_type
+
+      must_redirect_to recipes_path([:apple])
+
+    end
+  end
 end
