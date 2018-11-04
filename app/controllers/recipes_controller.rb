c771919c264dccf9320f75ad10dc5110dd576c9b
Lindsay Terchin <lterchin@gmail.com>
2018-11-04 12:53:40 -0800

    added link to index in header

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,14 +3,11 @@ class RecipesController < ApplicationController
   def index
     ingredient = params[:input]
     @recipes = ApiMuncherWrapper.recipes_by_ingredient(ingredient).paginate(page: params[:page], per_page: 10)
-
   end
 
   def show
-
     @uri = params[:id]
     @recipe = ApiMuncherWrapper.recipe_by_uri(@uri)
-
   end
 
 end
