4216ca9c15bdf780b12697ce4d4d0e6393ff7f65
Valerie Gidzinski <valgidzi@gmail.com>
2018-11-04 13:45:56 -0800

    New test for #show - calls render_404 if @recipe nil

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -13,11 +13,20 @@ describe RecipesController do
     end
   end
 
-  it 'can get the show path' do
+  it 'can get the show path for valid id' do
     VCR.use_cassette("recipes") do
       valid_id = "888e9fc4a808e9e4ccdb2ac24a6a2f46"
       get recipe_path(valid_id)
       must_respond_with :ok
+    end
+  end
+
+  it 'calls render_404 if recipe id is invalid' do
+    VCR.use_cassette("recipes") do
+      invalid_id = "123"
+      expect {
+        get recipe_path(invalid_id)
+      }.must_raise(ActionController::RoutingError)
     end
   end
 end
