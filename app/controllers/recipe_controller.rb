b4a71cc346b3c969aa9a01441d203c66cd40465a
Naheed Arang <arang.na@gmail.com>
2018-11-03 23:49:16 -0700

    delete controller

--- app/controllers/recipe_controller.rb
+++ app/controllers/recipe_controller.rb
@@ -1,16 +1,34 @@
 class RecipeController < ApplicationController
 
-  def search
-    @recipes = EdamamApiWrapper.list_recipes(:keyword)
-  end
+  # def search
+  #   @recipes = EdamamApiWrapper.list_recipes(:keyword)
+  # end
 
+  # def index
+  #   @keyword = params[:keyword]
+  #   @recipes = EdamamApiWrapper.list_recipes(@keyword).paginate(page: params[:page], per_page: 10)
+  # end
+  #
+  #   def index
+  #       @keyword = params[:keyword]
+  #        recipes = EdamamApiWrapper.list_recipes(params[:keyword])
+  #       @recipes = recipes.paginate(:page => params[:page], :per_page => 10)
+  #       if !recipes.any?
+  #         render :index, status: :bad_request
+  #       end
+  #     end
+  #
+  #
+  # end
+  # @search = params[:label]
+  #    @recipes = EdamamApiWrapper.list_recipes(params[:label])
+  #    @recipes = Kaminari.paginate_array(@recipes).page(params[:page]).per(10)
   def index
     @keyword = params[:keyword]
-    @recipes = EdamamApiWrapper.search_recipes(@keyword).paginate(page: params[:page], per_page: 10)
+    recipes = EdamamApiWrapper.list_recipes(params[:keyword])
+    @recipes = recipes.paginate(:page => params[:page], :per_page => 10)
+    if !recipes.any?
+      render :index, status: :bad_request
+    end
   end
-
-
 end
-# @search = params[:label]
-#    @recipes = EdamamApiWrapper.list_recipes(params[:label])
-#    @recipes = Kaminari.paginate_array(@recipes).page(params[:page]).per(10)
