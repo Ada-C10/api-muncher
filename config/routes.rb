f6a3aa2cae4b3d8b9c48db3133b529eaebfb1cb8
Valerie Gidzinski <valgidzi@gmail.com>
2018-11-04 01:53:02 -0800

    Recipes#show route

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'recipes#root'
   get '/recipes', to: 'recipes#index', as: 'recipes'
+  get '/recipe/:id', to: 'recipes#show', as: 'recipe'
 end
