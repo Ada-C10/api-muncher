214a255c08c73394022e27ff7d3ab04536f4d4d7
Addison Cole <addison.cole85@gmail.com>
2018-11-04 13:38:59 -0800

    add error message to index search, add flash to index, show and search page views, will now let you know if no results from search.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,8 +3,14 @@ class RecipesController < ApplicationController
 
   def index
     recipes = EdamamApiWrapper.search_recipes(params[:search])
-    @recipes = recipes.paginate(:page => params[:page], :per_page => 10)
-    @search_param = params[:search]
+    if recipes.empty?
+      flash[:error] = "No grub found. Please refine your search."
+      redirect_back fallback_location: root_path
+    else
+      @recipes = recipes.paginate(:page => params[:page], :per_page => 10)
+      @search_param = params[:search]
+    end
+
   end
 
   def show
