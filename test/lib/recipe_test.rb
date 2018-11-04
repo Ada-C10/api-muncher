3ea3e48232466412704c356a724a3ba5cfc08916
Chantelle Belic <chantelle.belic@gmail.com>
2018-11-03 21:03:46 -0700

    all tests passing for recipe class

--- test/lib/recipe_test.rb
+++ test/lib/recipe_test.rb
@@ -22,12 +22,42 @@ describe 'Recipe' do
   end
 
   it 'must have default value for options' do
-recipe = Recipe.new("bologne")
-    expect(recipe.ingredients).wont_be_nil
+    recipe = Recipe.new("bologne")
+    empty_list = ["N/A"]
+    empty_string = ""
+    missing_image = "http://vyfhealth.com/wp-content/uploads/2015/10/yoga-placeholder1.jpg"
+
+    expect(recipe.ingredients).must_equal empty_list
+    expect(recipe.uri).must_equal empty_string
+    expect(recipe.url).must_equal empty_string
+    expect(recipe.dietary_info).must_equal empty_list
+    expect(recipe.health_info).must_equal empty_list
+    expect(recipe.image_url).must_equal missing_image
   end
 
   it 'must be able to populate options' do
-  end
+    options = {
+      ingredients: ["ingredients"],
+      uri: ["uri"],
+      url: ["url"],
+      dietary_info: ["dietary_info"],
+      image_url: "image_url",
+      health_info: ["health_info"]
+    }
+    recipe = Recipe.new("bologne", options)
 
+    expect recipe.must_respond_to :ingredients
+    expect(recipe.ingredients).must_equal ["ingredients"]
+    expect recipe.must_respond_to :uri
+    expect(recipe.uri).must_equal ["uri"]
+    expect recipe.must_respond_to :url
+    expect(recipe.url).must_equal ["url"]
+    expect recipe.must_respond_to :dietary_info
+    expect(recipe.dietary_info).must_equal ["dietary_info"]
+    expect recipe.must_respond_to :image_url
+    expect(recipe.image_url).must_equal "image_url"
+    expect recipe.must_respond_to :health_info
+    expect(recipe.health_info).must_equal ["health_info"]
+  end
 
 end
