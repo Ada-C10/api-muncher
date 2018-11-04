fe9af913ea838b0ba8c1913844e997081c16e687
Layla Bristol <layla.bristol@gmail.com>
2018-11-03 22:21:59 -0700

    basic html pages set up

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,11 @@
+class RecipesController < ApplicationController
+
+  def index
+    @recipes
+  end
+
+  def show
+  end 
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  root 'main#index'
+  # get ':recipe/new', to: 'recipe#new', as: 'recipe_new'
+  # # get ':recipe', to: 'recipe#show', as: 'recipe_show'
+
+  resources :recipes do
+    end
+    
 end
