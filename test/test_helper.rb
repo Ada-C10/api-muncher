fafb2715838362fc2fd6248936feed38a6eea55b
Semret Nicodimos <snicodimos@gmail.com>
2018-11-04 14:33:34 -0800

    added test helper info in test_helper.rb

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,23 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
-require "minitest/rails"require "minitest/reporters"  # for Colorized output
+require "minitest/rails"
+require "minitest/reporters"  # for Colorized output
+require 'vcr'
+require 'webmock/minitest'
+
+# MonkeyPatch for This version of Rails :(
+if ActionPack::VERSION::STRING >= "5.2.0"
+  Minitest::Rails::TestUnit = Rails::TestUnit
+end
+
+# To add Capybara feature tests add `gem "minitest-rails-capybara"`
+# to the test group in the Gemfile and uncomment the following:
+# require "minitest/rails/capybara"
+
+# Uncomment for awesome colorful output
+# require "minitest/pride"
+
 #  For colorful output!
 Minitest::Reporters.use!(
   Minitest::Reporters::SpecReporter.new,
@@ -10,12 +26,32 @@ Minitest::Reporters.use!(
 )
 
 
-# To add Capybara feature tests add `gem "minitest-rails-capybara"`
-# to the test group in the Gemfile and uncomment the following:
-# require "minitest/rails/capybara"
+VCR.configure do |config|
+  config.cassette_library_dir = 'test/cassettes'
+  config.hook_into :webmock
+  config.default_cassette_options = {
+    :record => :new_episodes,
+    :match_requests_on => [:method, :uri, :body]
+  }
+  config.filter_sensitive_data("<APP_ID>") do
+    ENV["app_id"]
 
-# Uncomment for awesome colorful output
-# require "minitest/pride"
+  end
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'test/cassettes'
+  config.hook_into :webmock
+  config.default_cassette_options = {
+    :record => :new_episodes,
+    :match_requests_on => [:method, :uri, :body]
+  }
+  config.filter_sensitive_data("<APP_KEY>") do
+    ENV["app_key"]
+
+  end
+end
+
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
