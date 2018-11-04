63f4a42df4511c1d3b370aa2cb7370d70266b5de
Cassandra Archibald <cassandra.l.archibald@gmail.com>
2018-11-04 10:30:55 -0800

    Refactored recipe tests, changed id from an accessor to a reader

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,16 +1,10 @@
 require 'httparty'
 
 class Recipe
-  # Mixin - Can now do get/post requests like you would with httparty.
-  # Could just type in the HTTParty
-  # include HTTParty
-  # format :json
-  # base_uri ''
 
-  attr_reader :name, :image, :uri, :url, :ingredients, :dietLabels
-  attr_accessor :id
+  attr_reader :name, :image, :uri, :url, :ingredients, :dietLabels, :id
 
-  def initialize(label, image, uri, url, ingredients, id, dietLabels = [] )
+  def initialize(label, image, uri, url, ingredients, dietLabels = [] )
 
     raise ArgumentError if label == nil || label.empty?
     raise ArgumentError if image == nil || image.empty?
--- test/lib/recipe_test.rb
+++ test/lib/recipe_test.rb
@@ -1,101 +1,65 @@
 require 'test_helper'
 
 describe "Recipe" do
+    let(:recipe_name) { "Teriyaki Chicken" }
+    let(:image) {  "https://www.edamam.com/web-img/262/262b4353ca25074178ead2a07cdf7dc1.jpg" }
+    let(:uri) { "http://www.edamam.com/ontologies/edamam.owl#recipe_7bf4a371c6884d809682a72808da7dc2" }
+    let(:url) { "http://www.davidlebovitz.com/2012/12/chicken-teriyaki-recipe-japanese-farm-food/" }
+    let(:ingredients) { [ {"text" =>"1/2 cup olive oil"}, {"text" => "2lbs chicken"} ] }
+    let(:id) {
+      ( URI.encode ( "http://www.edamam.com/ontologies/edamam.owl#recipe_7bf4a371c6884d809682a72808da7dc2" ) )[/[^_]+$/]
+    }
+    let(:dietLabels) { dietLabels = ["vegan", "egg-free"] }
+
   it "Cannot be initialized without required parameters" do
+    # Missing all
     expect {
       Recipe.new
     }.must_raise ArgumentError
 
+    # Missing all but name
     expect {
-      Recipe.new "Chicken"
+      Recipe.new recipe_name
     }.must_raise ArgumentError
-   #
+
+   # Missing uri, url, ingredients
     expect {
-      Recipe.new "Chicken", "https://www.edamam.com/web-img/262/262b4353ca25074178ead2a07cdf7dc1.jpg"
+      Recipe.new recipe_name, image
     }.must_raise ArgumentError
-   #
+
+   # Missing url, ingredients
     expect {
-      Recipe.new
-      "Chicken"
-      "https://www.edamam.com/web-img/262/262b4353ca25074178ead2a07cdf7dc1.jpg"
-      "http://www.edamam.com/ontologies/edamam.owl#recipe_7bf4a371c6884d809682a72808da7dc2"
-   }.must_raise ArgumentError
-   #
-    expect {
-      Recipe.new
-        "Chicken"
-        "https://w.edamam.com/web-img/262/262b4353ca25074178ead2a07cdf7dc1.jpg"
-        "http://www.edamam.com/ontologies/edamam.owl#recipe_7bf4a371c6884d809682a72808da7dc2"
-        "http://www.davidlebovitz.com/2012/12/chicken-teriyaki-recipe-japanese-farm-food/"
-     }.must_raise ArgumentError
-   #
-    expect {
-      Recipe.new
-        "Chicken"
-        "https://www.edamam.com/web-img/262/262b4353ca25074178ead2a07cdf7dc1.jpg"
-        "http://www.edamam.com/ontologies/edamam.owl#recipe_7bf4a371c6884d809682a72808da7dc2"
-        "http://www.davidlebovitz.com/2012/12/chicken-teriyaki-recipe-japanese-farm-food/"
+      Recipe.new recipe_name, image, uri
     }.must_raise ArgumentError
-   #
+
+   # missing ingredients
     expect {
-      Recipe.new
-        "Chicken"
-        "https://www.edamam.com/web-img/262/262b4353ca25074178ead2a07cdf7dc1.jpg"
-        "http://www.edamam.com/ontologies/edamam.owl#recipe_7bf4a371c6884d809682a72808da7dc2"
-        "http://www.davidlebovitz.com/2012/12/chicken-teriyaki-recipe-japanese-farm-food/"
-        [
-          {"text" =>"1/2 cup olive oil"},
-          {"text" => "2lbs chicken"}
-        ]
+        Recipe.new recipe_name, image, uri, url
      }.must_raise ArgumentError
 
-     expect {
-       Recipe.new
-         "Chicken"
-         "https://www.edamam.com/web-img/262/262b4353ca25074178ead2a07cdf7dc1.jpg"
-         "http://www.edamam.com/ontologies/edamam.owl#recipe_7bf4a371c6884d809682a72808da7dc2"
-         "http://www.davidlebovitz.com/2012/12/chicken-teriyaki-recipe-japanese-farm-food/"
-         [
-           {"text" =>"1/2 cup olive oil"},
-           {"text" => "2lbs chicken"}
-         ]
-         "7bf4a371c6884d809682a72808da7dc2"
-      }.must_raise ArgumentError
 
   end
 
   it "Must initialize name, image, uri, url, ingredients, and id properly" do
-
-      # recipe = Recipe.new(
-      #   "Teriyaki Chicken",
-      #   "https://www.edamam.com/web-img/262/262b4353ca25074178ead2a07cdf7dc1.jpg",
-      #   "http://www.edamam.com/ontologies/edamam.owl#recipe_7bf4a371c6884d809682a72808da7dc2",
-      #   "http://www.davidlebovitz.com/2012/12/chicken-teriyaki-recipe-japanese-farm-food/",
-      #   [{"text"=>"1/2 cup olive oil", "weight"=>108.0},
-      #     {"text"=>"5 cloves garlic, peeled", "weight"=>15.0}]
-      #     [/[^_]+$/]
-      # )
-
-      recipe =  Recipe.new( "name", "image", "http://www.edamam.com/ontologies/edamam.owl#recipe_7bf4a371c6884d809682a72808da7dc2", "url", "ingredients", ( URI.encode  ( "http://www.edamam.com/ontologies/edamam.owl#recipe_7bf4a371c6884d809682a72808da7dc2" ) )[/[^_]+$/] )
-
-      expect(recipe.name).must_equal "name"
-      expect(recipe.image).must_equal "image"
-      expect(recipe.uri).must_equal "http://www.edamam.com/ontologies/edamam.owl#recipe_7bf4a371c6884d809682a72808da7dc2"
-      expect(recipe.url).must_equal "url"
-      expect(recipe.ingredients).must_equal "ingredients"
-      expect(recipe.id).must_equal ( URI.encode  ( "http://www.edamam.com/ontologies/edamam.owl#recipe_7bf4a371c6884d809682a72808da7dc2" ) )[/[^_]+$/]
+      recipe = Recipe.new(recipe_name, image, uri, url, ingredients)
+      expect(recipe.name).must_equal recipe_name
+      expect(recipe.image).must_equal image
+      expect(recipe.uri).must_equal uri
+      expect(recipe.url).must_equal url
+      expect(recipe.ingredients).must_equal ingredients
+      expect(recipe.id).must_equal id
   end
 
   it "can have optional fields" do
     dietLabels = ["vegan", "egg-free"]
-    recipe =  Recipe.new( "name", "image", "http://www.edamam.com/ontologies/edamam.owl#recipe_7bf4a371c6884d809682a72808da7dc2", "url", "ingredients", ( URI.encode  ( "http://www.edamam.com/ontologies/edamam.owl#recipe_7bf4a371c6884d809682a72808da7dc2" ) )[/[^_]+$/], dietLabels )
+    recipe = Recipe.new(recipe_name, image, uri, url, ingredients, dietLabels)
 
-    expect(recipe.name).must_equal "name"
-    expect(recipe.image).must_equal "image"
-    expect(recipe.uri).must_equal "http://www.edamam.com/ontologies/edamam.owl#recipe_7bf4a371c6884d809682a72808da7dc2"
-    expect(recipe.url).must_equal "url"
-    expect(recipe.ingredients).must_equal "ingredients"
-    expect(recipe.id).must_equal ( URI.encode  ( "http://www.edamam.com/ontologies/edamam.owl#recipe_7bf4a371c6884d809682a72808da7dc2" ) )[/[^_]+$/]
+    expect(recipe.name).must_equal recipe_name
+    expect(recipe.image).must_equal image
+    expect(recipe.uri).must_equal uri
+    expect(recipe.url).must_equal url
+    expect(recipe.ingredients).must_equal ingredients
+    expect(recipe.id).must_equal id
     expect(recipe.dietLabels).must_equal dietLabels
 
   end
