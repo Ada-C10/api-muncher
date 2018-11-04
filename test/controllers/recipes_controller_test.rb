c3d15a941fa0303ef6a01b619f3b63d7df6d7c12
Jessie Zhang <zhang_jessie2017@outlook.com>
2018-11-03 22:25:27 -0700

    edit the recipe_test file

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -4,4 +4,29 @@ describe RecipesController do
   # it "must be a real test" do
   #   flunk "Need real tests"
   # end
+  it "can get the home path" do
+      VCR.use_cassette('list_recipes_file') do
+        get root_path
+
+        must_respond_with :ok
+      end
+    end
+
+    it "can get the index path" do
+      get recipes_path
+      must_respond_with :ok
+    end
+
+    it "can get the show patch" do
+      VCR.use_cassette('recipe_detail') do
+        get recipe_create_path("uri"), params: { message: 'Happy Halloween' }
+
+        must_redirect_to root_path
+      end
+    end
+
+
+
+
+
 end
