41d7226720c8021d4a024d45774806f7277e0738
Lindsay Terchin <lterchin@gmail.com>
2018-11-03 22:43:06 -0700

    removed unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'search#index'
+
   get 'recipe/show/:id', to: 'recipes#show', as: 'return_recipe'
-
-
-  resources 'recipes'
-
-
-  # get 'recipes#index', to: 'recipes#index', as: 'recipes'
+  
+  resources :recipes, only: [:index, :show]
 
 end
