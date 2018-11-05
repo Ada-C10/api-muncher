6141a00e570bce1cd200aaeb29367bf9b119a406
Layla Bristol <layla.bristol@gmail.com>
2018-11-04 18:48:12 -0800

    starting test stuff

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -4,4 +4,15 @@ class RecipesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  describe "search" do
+
+    it "can get search page" do
+      get root_path
+      must_respond_with :success
+    end
+    
+  end
+
+
 end
