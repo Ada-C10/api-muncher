582748ee92cccf5911478c03cfa5ee812dd5ee76
Karis Kim <ouniiq@hotmail.com>
2018-11-05 11:35:37 -0800

    added more tests

--- test/lib/edamam_api_wrapper_test.rb
+++ test/lib/edamam_api_wrapper_test.rb
@@ -10,23 +10,44 @@ describe EdamamApiWrapper do
       end
     end
     #
-    # it "is successful when sending a message to a fake channel" do
-    #   VCR.use_cassette("recipes") do
-    #     is_successful = EdamamApiWrapper.send_message("this-channel-does-not-exist", "test message")
-    #     expect(is_successful).must_equal true
-    #   end
-    # end
-
+    it "is not successful when sending an invalid recipe name" do
+      VCR.use_cassette("recipes") do
+        all_recipes = EdamamApiWrapper.search("asdf")
+        expect(all_recipes.length).must_equal 0
+      end
+    end
   end
 
   describe "show recipe" do
-    it "" do
+    it "shows one instance of the recipe for valid recipe id" do
+      VCR.use_cassette("recipes") do
+        all_recipes = EdamamApiWrapper.search("cookie")
+        is_successful = EdamamApiWrapper.show_recipe("#{all_recipes[0].id}")
+        expect([is_successful].length).must_equal 1
+        expect(is_successful).must_be_instance_of Recipe
+      end
+    end
 
+    it "does not show one instance of the recipe for valid recipe id" do
+      VCR.use_cassette("recipes") do
+        all_recipes = EdamamApiWrapper.search("cookie")
+        is_successful = EdamamApiWrapper.show_recipe("#{all_recipes[0].id}")
+        expect([is_successful].length).must_equal 1
+        expect(is_successful).must_be_instance_of Recipe
+      end
     end
   end
 
   describe "create recipe " do
+    it "does not create an instance of Recipe if recipe is nil" do
+      VCR.use_cassette("recipes") do
+        recipe = nil
+        # binding.pry
+        test_case = EdamamApiWrapper.create_recipe(recipe)
 
+        expect(test_case).must_equal nil
+      end
+    end
   end
 
   describe "id from uri" do
