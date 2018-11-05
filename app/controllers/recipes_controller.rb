d9e4604e6062daf3a3b930b50ede60c55f7278e8
Karis Kim <ouniiq@hotmail.com>
2018-11-04 19:07:14 -0800

    images lined up well but image size not all the same, could not get search bar to work for index and show page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,23 +1,18 @@
 class RecipesController < ApplicationController
   def index
-    # raise
     @search_term = params[:recipes]
     @recipes = EdamamApiWrapper.search(params[:recipes]).paginate(:page => params[:page], :per_page => 10)
-
-    # raise
   end
 
   def new
-    # @recipe = params[:author]
-      # raise
+    @recipe = params[:keyword]
   end
 
   def create
-    redirect_to recipes_path(params[:author])
+    redirect_to recipes_path(params[:keyword])
   end
 
   def show
-# (params[:recipe].id)
     @recipe = EdamamApiWrapper.show_recipe(params[:recipe])
     if @recipe.diet_labels == []
       @recipe.diet_labels << "N/A"
