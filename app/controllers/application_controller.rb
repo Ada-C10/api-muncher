14ef35108381a5daa3b8c5cae0e9cada1123bef8
Daniela Di Iorio <danielad@gmail.com>
2018-11-04 19:15:17 -0800

    labels show up on search bar, form updated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,39 +1,44 @@
 class ApplicationController < ActionController::Base
 
-  @HEALTH_LABELS =
-    [
-      "balanced",
-      "high-fiber",
-      "high-protein",
-      "low-carb",
-      "low-fat",
-      "low-sodium",
-      "alcohol-free",
-      "celery-free",
-      "crustacean-free",
-      "dairy-free",
-      "egg-free",
-      "fish-free",
-      "gluten-free",
-      "kidney-friendly",
-      "kosher",
-      "low-potassium",
-      "lupine-free",
-      "mustard-free",
-      "No-oil-added",
-      "low-sugar",
-      "paleo",
-      "peanut-free",
-      "pescatarian",
-      "pork-free",
-      "red-meat-free",
-      "sesame-free",
-      "shellfish-free",
-      "soy-free",
-      "sugar-conscious",
-      "tree-nut-free",
-      "vegan",
-      "vegetarian",
-      "wheat-free"
-    ]
+  before_action :get_health_labels
+
+  def get_health_labels
+    @HEALTH_LABELS =
+      [
+        "balanced",
+        "high-fiber",
+        "high-protein",
+        "low-carb",
+        "low-fat",
+        "low-sodium",
+        "alcohol-free",
+        "celery-free",
+        "crustacean-free",
+        "dairy-free",
+        "egg-free",
+        "fish-free",
+        "gluten-free",
+        "kidney-friendly",
+        "kosher",
+        "low-potassium",
+        "lupine-free",
+        "mustard-free",
+        "No-oil-added",
+        "low-sugar",
+        "paleo",
+        "peanut-free",
+        "pescatarian",
+        "pork-free",
+        "red-meat-free",
+        "sesame-free",
+        "shellfish-free",
+        "soy-free",
+        "sugar-conscious",
+        "tree-nut-free",
+        "vegan",
+        "vegetarian",
+        "wheat-free"
+      ]
+  end
+
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -6,8 +6,9 @@ require 'will_paginate/array'
 
   def index
     @keyword = params[:keyword]
+    @health_labels = params_health_labels
 
-    recipes = EdamamApiWrapper.recipe_search(params[:keyword])
+    recipes = EdamamApiWrapper.recipe_search(@keyword, @health_labels)
     @recipes = recipes.paginate(:page => params[:page], :per_page => 10)
     if !recipes.any?
       render :index, status: :bad_request
@@ -19,6 +20,17 @@ require 'will_paginate/array'
     if @recipe.nil?
       render :show, status: :bad_request
     end
+  end
+
+private
+  def params_health_labels
+    health_labels = []
+    @HEALTH_LABELS.each do |label|
+      if params[label] == "1"
+        health_labels << label
+      end
+    end
+    return health_labels
   end
 
 end
