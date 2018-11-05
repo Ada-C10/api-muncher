9e26141fbd4c03ac954564162b8cc3f1384b5947
Karis Kim <ouniiq@hotmail.com>
2018-11-05 00:18:32 -0800

    made errors for entering invalid recipe name show up in root page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,20 +4,16 @@ class RecipesController < ApplicationController
 
     @search_term = params[:recipes]
     @recipes = EdamamApiWrapper.search(@search_term).paginate(:page => params[:page], :per_page => 10)
+    if @recipes.empty?
+      flash[:error] = "Could not find recipes. Try again."
+      redirect_back fallback_location: root_path
+    end
     # return
     # binding.pry
   end
 
-  # def new
-  # end
-  #
-  # def create
-  #   redirect_to recipes_path(params[:keyword])
-  # end
-
   def search
     @search = params[:recipes]
-    params.permit!(:recipes)
     if @search
       redirect_to recipes_index_path(params[:recipes])
       return
