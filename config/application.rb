c7a806e8dc5a5dc5ed0aed38363e265add37bf36
kangazoom <michellelcronin@gmail.com>
2018-11-05 05:07:47 -0800

    h10 error

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
+require 'httparty'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -16,7 +17,7 @@ module ApiMuncher
   end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    
+
     config.eager_load_paths << Rails.root.join('lib')
 
     # Settings in config/environments/* take precedence over those specified here.
