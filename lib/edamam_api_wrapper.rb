97c92f6eda58eb2fb536fb73ec4eedf0eb849bbb
Naheed Arang <arang.na@gmail.com>
2018-11-05 09:56:34 -0800

    show and index working, fixed find_recipe method

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -14,7 +14,7 @@ class EdamamApiWrapper
       label = recipe["recipe"]["label"]
       image = recipe["recipe"]["image"]
       uri = recipe["recipe"]["uri"]
-      recipe_list << create_recipe(recipe)
+      recipe_list << create_recipe(recipe["recipe"])
     end
     return recipe_list
   end
@@ -29,6 +29,16 @@ class EdamamApiWrapper
     end
   end
 
+  # def self.single_recipe(id)
+  #    url= BASE_URL + "search?r=http%3A%2F%2Fwww.edamam.com%2Fontologies%2Fedamam.owl%23recipe_#{id}" + "&app_id=#{ID}&app_key=#{KEY}"
+  #   data = HTTParty.get(url)
+  #    if data.empty?
+  #     return []
+  #   else
+  #    return create_recipe(data[0])
+  # end
+  #
+  #
   private
 
   def self.create_recipe(api_params)
