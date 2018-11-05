0ff9204986075729ec2a70cfd698feb2b2ec59f8
Semret Nicodimos <snicodimos@gmail.com>
2018-11-04 16:45:39 -0800

    added controller tests

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,7 +1,27 @@
 require "test_helper"
 
 describe RecipesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  describe "index and show" do
+    it 'root page is the new search page' do
+      get root_path
+
+      must_respond_with :ok
+    end
+
+    it 'should respond with a success ok for getting recipe list' do
+      get recipe_index_path(params[:word])
+
+      must_respond_with :ok
+
+    end
+
+    it 'should respond with a success ok for getting details of a recipe' do
+      get recipe_detail_path(params[:word])
+
+      must_respond_with :ok
+
+    end
+
+
+  end
 end
