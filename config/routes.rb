c8a1810c8c7346a3b07b39dd62fab71fff4bcf47
Karis Kim <ouniiq@hotmail.com>
2018-11-06 05:15:53 -0800

    fixed form not working in index and show page. completed all testing

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   root 'recipes#search'
 
-  get ':recipes/index', to: 'recipes#index', as: 'recipes_index'
+  get 'recipes', to: 'recipes#index', as: 'recipes_index'
 
-  get ':recipe/show', to: 'recipes#show', as: 'recipe_show'
+  get 'recipe/:recipe', to: 'recipes#show', as: 'recipe_show'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- test/lib/edamam_api_wrapper_test.rb
+++ test/lib/edamam_api_wrapper_test.rb
@@ -6,6 +6,7 @@ describe EdamamApiWrapper do
     it "returns correct number of recipes when a valid recipe word was entered" do
       VCR.use_cassette("recipes") do
         is_successful = EdamamApiWrapper.search("cookie")
+
         expect(is_successful.length).must_equal 50
       end
     end
@@ -20,15 +21,6 @@ describe EdamamApiWrapper do
 
   describe "show recipe" do
     it "shows one instance of the recipe for valid recipe id" do
-      VCR.use_cassette("recipes") do
-        all_recipes = EdamamApiWrapper.search("cookie")
-        is_successful = EdamamApiWrapper.show_recipe("#{all_recipes[0].id}")
-        expect([is_successful].length).must_equal 1
-        expect(is_successful).must_be_instance_of Recipe
-      end
-    end
-
-    it "does not show one instance of the recipe for valid recipe id" do
       VCR.use_cassette("recipes") do
         all_recipes = EdamamApiWrapper.search("cookie")
         is_successful = EdamamApiWrapper.show_recipe("#{all_recipes[0].id}")
@@ -51,10 +43,18 @@ describe EdamamApiWrapper do
   end
 
   describe "id from uri" do
+    it "must get the id from the uri" do
+      uri = "http://www.edamam.com/ontologies/edamam.owl#recipe_115b7da63f48e228cfdbf88e0e0a338a"
+      formatted_uri = EdamamApiWrapper.id_from_uri(uri)
 
+      expect(formatted_uri).must_equal "115b7da63f48e228cfdbf88e0e0a338a"
+    end
   end
 
   describe "uri from id" do
-
+    it "must combine url with id" do
+      result = EdamamApiWrapper.uri_from_id("115b7da63f48e228cfdbf88e0e0a338a")
+      expect(result).must_equal "http://www.edamam.com/ontologies/edamam.owl#recipe_115b7da63f48e228cfdbf88e0e0a338a"
+    end
   end
 end
