799b277cd9ef556fa3c02571ab51a568f94e61c9
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-11-05 20:31:26 -0800

    search working

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,16 +1,27 @@
 class RecipesController < ApplicationController
+  before_action :find_query
 
   def index
     @recipes = EdamamApiWrapper.search_recipes(params[:query])
+
+    if @recipes.count == 0
+      flash.now[:errror] = "Sorry, there are no recipes for that ingredient."
+    end
+
   end
 
   def show
+    @recipe = EdamamApiWrapper.recipe_contents(params[:id])
+
+    if @recipe.nil?
+      render :show, status: :not_found
+    end
   end
 
   private
 
-  def find_recipe_uri
-    @uri = EdamamApiWrapper.get_one_recipe(params[:recipe_id])
+  def find_query
+    @query = params[:query]
   end
 
 end
