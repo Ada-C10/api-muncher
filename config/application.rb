4af01479fadad05db9aac8dc2ae35774fe6a0f79
Divya <divyamohanan@gmail.com>
2018-11-07 15:37:39 -0800

    MAde changes to help config

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,6 @@ module ApiMuncher
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.autoload_paths += ['lib']
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
