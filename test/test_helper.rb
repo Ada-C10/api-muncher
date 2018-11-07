52e98991dd504ae7ebbaca70163ab78a28718261
Katricia Smith <smithkatricia@gmail.com>
2018-11-07 08:08:45 -0800

    Add VCR config to test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,12 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path("../../config/environment", __FILE__)
 require "rails/test_help"
-require "minitest/rails"require "minitest/reporters"  # for Colorized output
+require "minitest/rails"
+require "minitest/reporters"
+require 'vcr'
+require 'webmock/minitest'
+
+# for Colorized output
 #  For colorful output!
 Minitest::Reporters.use!(
   Minitest::Reporters::SpecReporter.new,
@@ -16,6 +21,24 @@ Minitest::Reporters.use!(
 
 # Uncomment for awesome colorful output
 # require "minitest/pride"
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'test/cassettes'
+  config.hook_into :webmock
+  config.default_cassette_options = {
+    :record => :new_episodes,
+    :match_requests_on => [:method, :uri, :body]
+  }
+
+  config.filter_sensitive_data("app_id") do
+    ENV['EDAMAM_APP_ID']
+  end
+
+  config.filter_sensitive_data("app_keys") do
+    ENV['EDAMAM_APP_KEYS']
+  end
+
+end
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
