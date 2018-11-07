a1c24af798ac08a0190f7c372deab5730f5f61af
Leanne Rivera <rivera.leanne+ada@gmail.com>
2018-11-07 12:57:07 -0800

    require pry removed

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -1,6 +1,4 @@
 require 'httparty'
-require 'awesome_print'
-require 'pry'
 
 class EdamamApiWrapper
    #example url search: "https://api.edamam.com/search?q=chicken&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free"
