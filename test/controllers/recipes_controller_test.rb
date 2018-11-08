d25f373ecfdf1a707dcdb66018efd3b6f87c8d6d
Katricia Smith <smithkatricia@gmail.com>
2018-11-07 19:11:05 -0800

    Add recipes controller tests for index and search

--- test/controllers/recipes_controller_test.rb
+++ test/controllers/recipes_controller_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 describe RecipesController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
+  # it "can get the index path" do
+  #   VCR.use_cassette("recipes") do
+  #     get root_path
+  #
+  #     must_respond_with :ok
+  #   end
+  # end
+  #
+  # it "can get list of recipes for user search term" do
+  #   VCR.use_cassette('recipes') do
+  #     get recipes_path, params: { q: 'lemongrass'}
+  #
+  #     must_respond_with :ok
+  #   end
   # end
 end
